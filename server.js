8f88e24800eb45e7f726025dd4d5ec495082fdfe
Pedro Miguel <pedro.miguel@live.co.uk>
2019-04-10 15:51:36 +0100

    Update app entry point to use routes

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ const cors = require('cors')
 const express = require('express');
 
 // local imports
+const routes = require('./routes');
 const config = require('./config/core');
 const logger = require('./config/logger');
 
 const app = express();
 const port = config.PORT || 5000;
 
-app.get('', (req, res) => {
-  logger.info(`${req.url} - Requested`);
-  res.json({data: ''});
-});
+app.use(routes);
 
 app.listen(port, () => {
   logger.info(`Server listening on port ${port}`);
