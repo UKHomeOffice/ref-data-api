10e19da7c0b59408be6833de3f1d330f6f9de973
Pedro Miguel <pedro.miguel@live.co.uk>
2019-04-12 17:21:42 +0100

    Add mock/base patch endpoint for updating an item field

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,6 @@ router.get('/v1/entities/:name', entities.getEntity);
 router.patch('/v1/entities/:name', entities.patchEntitySchema);
 router.post('/v1/entities/:name', entities.postEntityItem);
 router.get('/v1/entities/:name/items/:id', items.getItem);
+router.patch('/v1/entities/:name/items/:id', items.patchItemField);
 
 module.exports = router
