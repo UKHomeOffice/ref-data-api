8a35961d864b3c94128d911f9bfa9ba6ebd4f18e
Pedro Miguel <pedro.miguel@live.co.uk>
2019-04-12 14:51:23 +0100

    Replace entities request call with axios

--- routes/entities.js
+++ routes/entities.js
@@ -3,48 +3,20 @@ const router = require('express').Router();
 // local imports
 const logger = require('../config/logger');
 const { getEntitiesData, getEntityData } = require('../services/entities');
+const { token } = require('../config/core');
 
-const token = '';
-
-/**
- * return all entities using the
- * token passed in the request headers
- */
-const getEntities = (req, res) => {
-  getEntitiesData(token, (error, response, data) => {
-    if (!error && response.statusCode === 200) {
-      const result = {
-        'status': response.statusMessage,
-        'code': response.statusCode,
-        'data': []
-      };
-
-      for (const [entity, item] of Object.entries(data.definitions)) {
-        const { summary } = data.paths['/' + entity].get;
-        const { description, schemalastupdated, dataversion } = JSON.parse(summary);
-
-        result['data'].push({
-          description,
-          dataversion,
-          'entityName': entity,
-          'label': '',
-          'schema': {
-            'description': item.description,
-            'required': item.required,
-            'properties': item.properties
-          },
-          'lastupdated': schemalastupdated
-        });
-      }
-      res.json(result);
-    } else {
-      res.json({
-        'code': response.statusCode,
-        'error': error,
-        'body': response.body
-      });
-    }
-  });
+const getEntities = async (req, res) => {
+  try {
+    const data = await getEntitiesData(token);
+    res.json(data);
+  } catch (error) {
+    logger.error(`Error: ${error}`);
+    res.json({
+      'code': error.response.status,
+      'status': error.response.statusText,
+      'data': error.response.data.message
+    });
+  }
 };
 
 /**
--- services/entities.js
+++ services/entities.js
@@ -1,27 +1,50 @@
+const axios = require('axios');
 const request = require('request');
 const util = require('util');
 
+const { jsonify } = require('../helpers');
 const { postgrestUrls } = require('../config/core');
 
-const getEntitiesData = (token, callback) => {
+/**
+ * getEntitiesData() returns all entities.
+ *
+ * @param {token} Keycloak token
+ * @return {result} JSON object
+ */
+function getEntitiesData(token) {
   const entitiesUrl = util.format(postgrestUrls.entities);
-  const options = {
-    method: 'GET',
-    url: entitiesUrl,
-    json: true,
+  return axios.get(entitiesUrl, {
     headers: {
       Authorization: `Bearer ${token}`
     }
-  };
+  })
+  .then(function (response) {
+    const result = {
+      'status': response.statusText,
+      'code': response.status,
+      'data': []
+    };
 
-  request(options, (error, response, body) => {
-    if (!error) {
-      callback(error, response, body);
-    } else {
-      callback(error, response, body);
+    for (const [entity, item] of Object.entries(response.data.definitions)) {
+      const { summary } = response.data.paths['/' + entity].get;
+      const { description, schemalastupdated, dataversion } = jsonify(summary);
+
+      result['data'].push({
+        description,
+        dataversion,
+        'entityName': entity,
+        'label': '',
+        'schema': {
+          'description': item.description,
+          'required': item.required,
+          'properties': item.properties
+        },
+        'lastupdated': schemalastupdated
+      });
     }
-  });
-};
+    return result;
+  })
+}
 
 const getEntityData = (token, name, callback) => {
   const entityUrl = util.format(postgrestUrls.entity, name);
