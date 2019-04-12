a91f72e8b1ae6a19fe85cdb3d2bb0407ae0099e7
Pedro Miguel <pedro.miguel@live.co.uk>
2019-04-12 11:12:37 +0100

    Add postgrest configuration endpoints

--- config/core.js
+++ config/core.js
@@ -1,12 +1,20 @@
 'use strict'
 
+const postgrestBaseUrl = process.env.POSTGREST_BASE_URL;
+const postgrestUrls = {
+  entities: postgrestBaseUrl,
+  entity: postgrestBaseUrl + '/%s',
+  item: postgrestBaseUrl + '/%s?id=eq.%s'
+};
+
 const config = {
+  postgrestUrls,
   logLevel: process.env.LOG_LEVEL || 'info',
   port: process.env.PORT || 5000,
   keycloakAuthUrl: process.env.KEYCLOAK_AUTH_URL,
   clientId: process.env.KEYCLOAK_CLIENT_ID,
   realm: process.env.KEYCLOAK_REALM,
-  postgrestBaseUrl: process.env.POSTGREST_BASE_URL
-}
+  token: ''
+};
 
 module.exports = config;
