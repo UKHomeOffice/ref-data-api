c2742aad69495199997ffd6250f05fec39d14b1a
Pedro Miguel <pedro.miguel@live.co.uk>
2019-05-10 14:35:11 +0100

    Update config with development settings

--- config/core.js
+++ config/core.js
@@ -1,4 +1,4 @@
-const postgrestBaseUrl = process.env.POSTGREST_BASE_URL;
+const postgrestBaseUrl = process.env.POSTGREST_BASE_URL || 'https://postgrest-dummy-url.co.uk';
 const postgrestUrls = {
   'entities': postgrestBaseUrl,
   'entity': `${postgrestBaseUrl}/%s`,
@@ -8,6 +8,7 @@ const postgrestUrls = {
 const config = {
   postgrestUrls,
   'logLevel': process.env.LOG_LEVEL || 'info',
+  'dbConnectionString': process.env.REF_DATA_DB_CONNECTION_STRING || 'postgres://user:pass@localhost:5432/dbname',
   'port': process.env.PORT || 5000,
 };
 
