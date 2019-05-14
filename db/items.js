476999e48ebc349d56f534a04963bfc2fdc9c367
Pedro Miguel <pedro.miguel@live.co.uk>
2019-05-14 10:59:32 +0100

    Add functionality to get and display an item

---
+++ db/items.js
@@ -0,0 +1,21 @@
+// local imports
+const logger = require('../config/logger');
+const pool = require('./index');
+
+const getItemData = (role, tableName, objId) => new Promise((resolve, reject) => {
+  const query = {
+    'text': `SELECT * FROM ${tableName} WHERE id=$1;`,
+    'values': [objId],
+  };
+  pool.query(`SET ROLE ${role};`)
+    .then(() => pool.query(query))
+    .then(data => resolve(data.rows[0]))
+    .catch((error) => {
+      const errorMsg = `Unable to retrieve data from table ${tableName} row id ${objId}`;
+      logger.error(errorMsg);
+      logger.error(error);
+      reject(new Error(errorMsg));
+    });
+});
+
+module.exports = { getItemData };
