ae02e36fb43a1af08ed9b50f034cb3468c09385e
Pedro Miguel <pedro.miguel@live.co.uk>
2019-05-17 22:39:46 +0100

    Update structure to use app routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,25 @@
-const router = require('express').Router();
+const cors = require('cors');
+const express = require('express');
+// const router = require('express').Router();
 
 // local imports
 const entities = require('./entities');
 const items = require('./items');
 
-router.get('/v1/entities', entities.getEntities);
-router.get('/v1/entities/:name', entities.getEntity);
-router.patch('/v1/entities/:name', entities.patchEntitySchema);
-router.post('/v1/entities/:name', entities.postEntityItem);
-router.get('/v1/entities/:name/items/:id', items.getItem);
-router.patch('/v1/entities/:name/items/:id', items.patchItemField);
+const app = express();
+const corsConfiguration = {
+  origin: '*',
+  methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsConfiguration));
+app.options('*', cors(corsConfiguration));
 
-module.exports = router;
+app.get('/v1/entities', entities.getEntities);
+app.get('/v1/entities/:name', entities.getEntity);
+app.patch('/v1/entities/:name', entities.patchEntitySchema);
+app.post('/v1/entities/:name', entities.postEntityItem);
+app.get('/v1/entities/:name/items/:id', items.getItem);
+app.patch('/v1/entities/:name/items/:id', items.patchItemField);
+
+module.exports = app;
