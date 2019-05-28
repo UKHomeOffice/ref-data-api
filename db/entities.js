fb561dbecf00f86a2939fe304311139aace83b22
Pedro Miguel <pedro.miguel@live.co.uk>
2019-05-28 22:24:07 +0100

    Remove unecessary new Promise statements

--- db/entities.js
+++ db/entities.js
@@ -2,51 +2,49 @@
 const logger = require('../config/logger');
 const pool = require('./index');
 
-const getEntityDescription = tableName => new Promise((resolve, reject) => {
+const getEntityDescription = (tableName) => {
   const query = {
     'text': `SELECT obj_description($1::regclass, 'pg_class')
             AS description;`,
     'values': [tableName],
   };
-  pool.query(query)
-    .then(data => resolve({ 'description': JSON.parse(data.rows[0].description) }))
+  return pool.query(query)
+    .then(data => ({ 'description': JSON.parse(data.rows[0].description) }))
     .catch((error) => {
       const errorMsg = `Unable to retrieve description from table ${tableName}`;
       logger.error(errorMsg);
       logger.error(error.stack);
-      reject(new Error(errorMsg));
+      return new Error(errorMsg);
     });
-});
+};
 
-const getEntitySchema = (role, entityName) => new Promise((resolve, reject) => {
-  pool.query(`SET ROLE ${role};`)
-    .then(() => pool.query(`SELECT column_name, is_nullable, data_type, character_maximum_length,
+const getEntitySchema = (role, entityName) => pool.query(`SET ROLE ${role};`)
+  .then(() => pool.query(`SELECT column_name, is_nullable, data_type, character_maximum_length,
       col_description((table_schema||'.'||table_name)::regclass::oid, ordinal_position) AS description
       FROM information_schema.columns WHERE table_name='${entityName}';`))
-    .then((data) => {
-      const required = [];
-      const properties = {};
+  .then((data) => {
+    const required = [];
+    const properties = {};
 
-      data.rows.forEach((obj) => {
-        if (obj.is_nullable === 'NO') {
-          required.push(obj.column_name);
-        }
-        properties[obj.column_name] = {
-          'maxLength': obj.character_maximum_length,
-          'format': obj.data_type,
-          'type': obj.data_type,
-          'description': JSON.parse(obj.description),
-        };
-      });
-      resolve({ required, properties });
-    })
-    .catch((error) => {
-      const errorMsg = `Unable to retrieve schema from table ${entityName}`;
-      logger.error(errorMsg);
-      logger.error(error.stack);
-      reject(new Error(errorMsg));
+    data.rows.forEach((obj) => {
+      if (obj.is_nullable === 'NO') {
+        required.push(obj.column_name);
+      }
+      properties[obj.column_name] = {
+        'maxLength': obj.character_maximum_length,
+        'format': obj.data_type,
+        'type': obj.data_type,
+        'description': JSON.parse(obj.description),
+      };
     });
-});
+    return { required, properties };
+  })
+  .catch((error) => {
+    const errorMsg = `Unable to retrieve schema from table ${entityName}`;
+    logger.error(errorMsg);
+    logger.error(error.stack);
+    return new Error(errorMsg);
+  });
 
 const getAllEntities = () => pool.query('SELECT * FROM pg_catalog.pg_tables WHERE schemaname = \'reference\';')
   .then((data) => {
@@ -61,17 +59,15 @@ const getAllEntities = () => pool.query('SELECT * FROM pg_catalog.pg_tables WHER
     return new Error(errorMsg);
   });
 
-const getEntityData = (role, entityName) => new Promise((resolve, reject) => {
-  pool.query(`SET ROLE ${role};`)
-    .then(() => pool.query(`SELECT * FROM ${entityName};`))
-    .then(data => resolve(data.rows))
-    .catch((error) => {
-      const errorMsg = `Unable to retrieve data from table ${entityName}`;
-      logger.error(errorMsg);
-      logger.error(error.stack);
-      reject(new Error(errorMsg));
-    });
-});
+const getEntityData = (role, entityName) => pool.query(`SET ROLE ${role};`)
+  .then(() => pool.query(`SELECT * FROM ${entityName};`))
+  .then(data => data.rows)
+  .catch((error) => {
+    const errorMsg = `Unable to retrieve data from table ${entityName}`;
+    logger.error(errorMsg);
+    logger.error(error.stack);
+    return new Error(errorMsg);
+  });
 
 module.exports = {
   getAllEntities,
