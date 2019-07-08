d150f75241b1de868b21aca23ffe343e35061c26
Pedro Miguel <pedro.miguel@live.co.uk>
2019-07-08 11:00:36 +0100

    Linting

--- healthcheck.js
+++ healthcheck.js
@@ -3,16 +3,18 @@ const request = require('request');
 // local imports
 const config = require('./config/core');
 
-const options = {  
-  url : `${config.hostname}/_health`,
-  timeout : 2000
+const options = {
+  'url': `${config.hostname}/_health`,
+  'timeout': 2000,
 };
 
 request(options, (error, response, body) => {
-  if (response.statusCode == 200) {
+  if (response.statusCode === 200) {
+    // eslint-disable-next-line no-console
     console.log(`STATUS: ${response.statusCode}`);
     process.exit(0);
   } else {
+    // eslint-disable-next-line no-console
     console.log(`ERROR: ${error}`);
     process.exit(1);
   }
