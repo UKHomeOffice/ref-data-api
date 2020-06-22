13fb84a22dc88c77cdcf89e5a869b1ba1746930a
Abi James <a.james1@live.co.uk>
2020-06-22 11:32:57 +0100

    Adding unit model/test

---
+++ test/db/models/unit.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const { Sequelize, DataTypes } = require('sequelize');
+
+const { Unit } = require('../../../app/db/models/unit');
+
+const sequelize = new Sequelize('database', 'username', 'password', { dialect: 'postgres' });
+
+describe('Given a Unit model', () => {
+  describe('when it is initialised', () => {
+    before(() => {
+      Unit.init(sequelize, DataTypes);
+    });
+
+    beforeEach(() => {
+      this.modelInstance = new Unit();
+    });
+    it('should have the correct tableName', () => {
+      expect(Unit.getTableName()).to.eql('unit');
+    });
+
+    ['id', 'unit', 'validfrom', 'validto'].forEach((column) => {
+      it(`should have the column: ${column}`, () => {
+        expect(this.modelInstance).to.have.property(column);
+      });
+    });
+  });
+});
