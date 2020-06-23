0ab258b99c49c13c87d278b7025d5a9de7198fd4
Belinda Cowey <belinda.cowey@digital.homeoffice.gov.uk>
2020-06-23 15:03:08 +0100

    COP-441 Model command schema

---
+++ test/models/command.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const { Sequelize, DataTypes } = require('sequelize');
+
+const { Command } = require('../../models/command');
+
+const sequelize = new Sequelize('database', 'username', 'password', { dialect: 'postgres' });
+
+describe('Given a Command model', () => {
+  describe('when it is initialised', () => {
+    before(() => {
+      Command.init(sequelize, DataTypes);
+    });
+
+    beforeEach(() => {
+      this.modelInstance = new Command();
+    });
+    it('should have the correct tableName', () => {
+      expect(Command.getTableName()).to.eql('command');
+    });
+
+    ['id', 'name', 'divisionid', 'code', 'operationalcommand', 'validfrom', 'validto'].forEach((column) => {
+      it(`should have the column: ${column}`, () => {
+        expect(this.modelInstance).to.have.property(column);
+      });
+    });
+  });
+});
